fa41eac2cd61de96ded78ec3082be18e8ed7c5bb
Hiroki Sakaji <hiroki_sakaji@st.seikei.ac.jp>
2013-12-24 17:44:23 +0900

    テストファイルを変更

--- src/cabochaParser/parserTest.java
+++ src/cabochaParser/parserTest.java
@@ -9,6 +9,9 @@ public class parserTest {
 		String test = "* 0 1D 0/1 0.000000\n今日\t名詞,副詞可能,*,*,*,*,今日,キョウ,キョー\nは\t助詞,係助詞,*,*,*,*,は,ハ,ワ\n* 1 -1D 0/0 0.000000\n晴れ\t名詞,一般,*,*,*,*,晴れ,ハレ,ハレ\nEOS";
 		CabochaParser parser = new CabochaParser(test);
 		ArrayList<POS> results = parser.parse();
+		for (POS pos : results) {
+			System.out.println(pos.str);
+		}
 		System.out.println("終了");
 	}
 
