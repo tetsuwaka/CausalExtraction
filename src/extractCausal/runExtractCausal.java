389944bed1b745a06c30c9e474b3ba619198756b
Hiroki Sakaji <tetsuwaka@gmail.com>
2025-07-23 09:56:30 +0900

    fix

--- src/extractCausal/runExtractCausal.java
+++ src/extractCausal/runExtractCausal.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
